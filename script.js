bbc07629c183bfaa8e54d5a935b5c3122a88fcbf
anvar <anvar>
2022-11-23 19:03:37 +0500

    anvarjs

---
+++ script.js
@@ -0,0 +1,82 @@
+var ism = prompt ("Ismingizni kiriting:")
+console.log("Siznig ismingiz: " + ism)
+var age = +prompt ("Yoshingizni kiriting:")
+console.log("Siznig yoshingiz: " + age)
+var yil = +prompt ("Tug`ilgan yilingizni kiriting:")
+console.log("Tug`ilgan yilingiz: " + yil)
+var joy = prompt ("Tug`ilgan joyingizni kiriting:")
+console.log("Tug`ilgan joyingiz: " + joy)
+var manzil = prompt ("Yashash manzilingizni kiriting:")
+console.log("Yashash manzilingiz: " + manzil)
+var maktab = prompt ("Qaysi maktabda yoki institut(universitut, litsey, kollej) o`qiysiz:")
+console.log("O`qish joyingiz: " + maktab)
+var qizla = +prompt ("nechta qiz yoki bola bilan gaplashgansiz:")
+console.log("gaplashgan qizlaringiz yoki bolaringiz soni:) " + qizla)
+var aniqlimiz = prompt ("kotmisiz yomi torisini eting:)")
+console.log("kotmasakansiz:) " + aniqlimiz)
+var misol = +prompt ("5+4=?")
+var javob = 5+4
+if (misol==javob) {
+    console.log("Sizning javobingiz to`g`ri: "+javob)
+}else{
+        console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+    }
+    var misol = +prompt ("888+888=?")
+var javob = 888+888
+if (misol==javob) {
+    console.log("Sizning javobingiz to`g`ri: "+javob)
+}else{
+        console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+    }
+    var misol = +prompt ("12-6=?")
+    var javob = 12-6
+    if (misol==javob) {
+        console.log("Sizning javobingiz to`g`ri: "+javob)
+    }else{
+            console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+        }
+        var misol = +prompt ("956-128=?")
+    var javob = 956-128
+    if (misol==javob) {
+        console.log("Sizning javobingiz to`g`ri: "+javob)
+    }else{
+            console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+        }
+        var misol = +prompt ("4*4=?")
+        var javob = 4*4
+        if (misol==javob) {
+            console.log("Sizning javobingiz to`g`ri: "+javob)
+        }else{
+                console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+            }
+            var misol = +prompt ("128*256=?")
+        var javob = 128*256
+        if (misol==javob) {
+            console.log("Sizning javobingiz to`g`ri: "+javob)
+        }else{
+                console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+            }
+            var misol = +prompt ("16/4=?")
+            var javob = 16/4
+            if (misol==javob) {
+                console.log("Sizning javobingiz to`g`ri: "+javob)
+            }else{
+                    console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+                }
+                var misol = +prompt ("800/4=?")
+            var javob = 800/4
+            if (misol==javob) {
+                console.log("Sizning javobingiz to`g`ri: "+javob)
+            }else{
+                    console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+                }
+                var misol = +prompt ("13%2=?")
+                var javob = 13%2
+                if (misol==javob) {
+                    console.log("Sizning javobingiz to`g`ri: "+javob)
+                }else{
+                        console.log("Sizning javobingiz noto`g`ri: "+misol + " to`g`ri javob esa: "+javob)
+                    }
+                    alert("natijani bilish uchun consoleni oching(F12)" )
+                    console.log("natija yomonmas:)")
+                
